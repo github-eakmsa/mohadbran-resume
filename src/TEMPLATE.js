902e7f0ced01695bfdba89f0ccf0628a567f0ed6
mohammedbrhan <mohammedbrhan@addwebsolution.in>
2024-04-10 06:35:37 +0300

    personal resume info added

--- src/TEMPLATE.js
+++ src/TEMPLATE.js
@@ -1,121 +1,176 @@
-
 // MUI: Icons
-import PhoneIcon from '@mui/icons-material/Phone';
-import EmailIcon from '@mui/icons-material/Email';
-import HomeIcon from '@mui/icons-material/Home';
-import FacebookIcon from '@mui/icons-material/Facebook';
-import InstagramIcon from '@mui/icons-material/Instagram';
-import LinkedInIcon from '@mui/icons-material/LinkedIn';
-import TwitterIcon from '@mui/icons-material/Twitter';
-import GithubIcon from '@mui/icons-material/GitHub';
-
+import PhoneIcon from "@mui/icons-material/Phone";
+import EmailIcon from "@mui/icons-material/Email";
+import HomeIcon from "@mui/icons-material/Home";
+import FacebookIcon from "@mui/icons-material/Facebook";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import TwitterIcon from "@mui/icons-material/Twitter";
+import GithubIcon from "@mui/icons-material/GitHub";
 
 // Data object that contains all of the data for the template.
 const data = {
-    profile: {
-        name: "Mohammedbrhan Abdelkadr",
-        address: "Addis Ababa, Ethiopia",
-        avatar: "https://mui.com/static/images/avatar/1.jpg",
-        contacts: [
-            { icon: PhoneIcon, label: "Phone", value: "(+251) 927 73 45 04" },
-            { icon: EmailIcon, label: "Email", value: "eakmsa@gmail.com" },
-            { icon: HomeIcon, label: "Address", value: "Kolfe Keraniyo, Addis Ababa, Ethiopia" }
-        ],
-        skills: [
-            { category: "Programming Languages:", skills: "C/C++, Java, Assembly" },
-            { category: "Scripting Languages:", skills: "Bash Shell, HTML" },
-            { category: "Operating Systems:", skills: "Unix/Linux, Mac OSX, Windows XP & Vista" },
-            { category: "Documentation:", skills: "MS Word, Excel, PowerPoint & Visio" },
-        ],
-        socialMedia: [
-            { icon: FacebookIcon, link: "facebook.com" },
-            { icon: InstagramIcon, link: "facebook.com" },
-            { icon: LinkedInIcon, link: "facebook.com" },
-            { icon: TwitterIcon, link: "facebook.com" },
-            { icon: GithubIcon, link: "facebook.com" },
-        ],
-        education: [
-            {
-                degree: "M.S. in Computer Science",
-                date: "Anticipated May 20xx",
-                school: "University of Massachusetts Lowell - Lowell, MA",
-                gpa: "Overall GPA: 3.33, GPA in Major: 3.50"
-            },
-            {
-                degree: "B.S. in Computer Science",
-                date: "Anticipated May 20xx",
-                school: "University of Massachusetts Lowell - Lowell, MA",
-                gpa: "Overall GPA: 3.33, GPA in Major: 3.50"
-            },
-        ],
-        experience: [
-            {
-                title: "Front End Developer / w3schools.com",
-                date: "Jan 2015 - Current",
-                description: "Lorem ipsum dolor sit amet...",
-            },
-            {
-                title: "Computer Lab Monitor / University of Massachusetts Lowell",
-                date: "Jan 20xx - Present",
-                description: "Monitor lab activity for up to 50 users at a time. Perform basic hardware/software troubleshooting.",
-            },
-            {
-                title: "Technical Support Specialist / Commonwealth of Massachusetts, Information Technology Division",
-                date: "Summer 20xx",
-                description: "Supported and hosted state-wide agency systems and messaging services on platforms including HP Unix, Linux, MVS, and Windows Active Exchange. Assisted with server builds and maintenance. Reviewed, installed, set up, and assisted with active synching of BlackBerry mobile devices to Windows Email Exchange server.",
-            },
-            {
-                title: "Asperger’s Association of New England",
-                date: "Jun. 20xx - Aug. 20xx",
-                description: "Volunteered one day a week with data entry and basic financial accounting projects",
-            },
-            {
-                title: "Lifeguard",
-                date: "Summers 20xx – 20xx",
-                description: "YMCA, Lowell, MA",
-            },
-        ],
-    },
+  profile: {
+    name: "Mohammedbrhan Abdelkadr",
+    address: "Addis Ababa, Ethiopia",
+    avatar: "profile-photo.png",
+    summary:
+      "I am a Fullstack Web Developer with 6+ years of work Experience in Analyzing, Designing, Developing and Integrating Front-End & Back-End based applications. I am capable of developing Back-End applications and APIs using PHP and MySQL. I also have Experience in developing Front-End applications and API clients using HTML, CSS, JavaScript, and Bootstrap. Currently, I am Learning and working with Laravel, Drupal, ReactJs, Nodejs, and ExpressJs heading towards the MERN-Stack. ",
+    contacts: [
+      { icon: PhoneIcon, label: "Phone", value: "(+251) 927 73 45 04" },
+      { icon: EmailIcon, label: "Email", value: "eakmsa@gmail.com" },
+      {
+        icon: HomeIcon,
+        label: "Address",
+        value: "Kolfe Keraniyo, Addis Ababa, Ethiopia",
+      },
+    ],
+    skills: [
+      {
+        category: "Programming Languages:",
+        skills: "PHP, Java, Python, JavaScript, Node.js",
+      },
+      { category: "Databases:", skills: "MySQL, Sqlite, MongoDB" },
+      { category: "Operating Systems:", skills: "Unix/Linux, Windows" },
+      { category: "Frontend:", skills: "HTML, CSS, JavaScript" },
+      {
+        category: "Web Development Frameworks:",
+        skills:
+          "Laravel, Drupal, Django, Spring, Express.js, React.js, Vue.js, Material UI, Bootstrap CSS",
+      },
+      {
+        category: "Tools:",
+        skills:
+          "VS Code, XAMPP, Phpmyadmin, Navicat-MySQL client, Postman, Composer, NPM, Git (GitHub), Docker, CPanel, Trello",
+      },
+      {
+        category: "Soft skills:",
+        skills:
+          "Communication, Teamwork, Critical Thinking, Positive Attitude, Work Ethic",
+      },
+    ],
+    socialMedia: [
+      { icon: FacebookIcon, link: "#" },
+      { icon: InstagramIcon, link: "#" },
+      { icon: TwitterIcon, link: "#" },
+      {
+        icon: LinkedInIcon,
+        link: "https://www.linkedin.com/in/mohammedbrhan-abdelkadr/",
+      },
+      { icon: GithubIcon, link: "https://github.com/github-eakmsa" },
+    ],
+    education: [
+      {
+        degree: "M.Sc. - in Data and Web Engineering in Computer Science",
+        date: "Sep 2018 - Oct 2021",
+        school: "Addis Ababa University - Addis Ababa, Ethiopia",
+        // gpa: "Overall GPA: 3.33, GPA in Major: 3.50"
+      },
+      {
+        degree: "B.Sc. - in Computer Science",
+        date: "Sep 2013 - Jul 2016",
+        school: "Mekelle University - Mekelle, Ethiopia",
+        gpa: "Overall GPA: 3.33, GPA in Major: 3.50",
+      },
+    ],
+    experience: [
+      {
+        title:
+          "Remote Software Engineer / AddWeb Solution private limited - Ahmedabad, India",
+        date: "Aug 2022 - Feb 2024",
+        description:
+          "Backend, frontend, REST API, Microservices using Laravel, Drupal",
+      },
+      {
+        title:
+          "Software Developer / Al-Afia Share Company - Addis Ababa, Ethiopia",
+        date: "July 2020 - Aug 2022",
+        description:
+          "Full stack Web development, training, technical support, and maintenance.",
+      },
+      {
+        title:
+          "Contract Web Developer / Al-Afia Share Company - Addis Ababa, Ethiopia",
+        date: "Aug 2019 - July 2020",
+        description:
+          "Full stack Web development, backend logic using PHP, database design and development using MySQL, Frontend development using HTML, CSS, Javascript, and Bootstrap CSS.",
+      },
+    ],
+    certifications: [
+      {
+        title: "JavaScript Tutorial Course / #1024-12400479 / Sololearn",
+        date: "2019",
+        description: "",
+      },
+      {
+        title: "PHP Tutorial Course / #1059-12400479 / Sololearn",
+        date: "2019",
+        description: "",
+      },
+      {
+        title: "Java Tutorial Course / #1059-12400479 / Sololearn",
+        date: "2019",
+        description: "",
+      },
+    ],
+    awards: [
+      {
+        title: "Certificate of Completion for Student Information Management System development / Al-Afia Share Company ",
+        date: "2021",
+        description: "",
+      },
+      {
+        title: "Certificate of Completion for Share Holder Information Management System development / Al-Afia Share Company ",
+        date: "2021",
+        description: "",
+      },
+      {
+        title: "Certificate of Completion for Video Production Management System development / Al-Habesha Media and Communication",
+        date: "2020",
+        description: "",
+      },
+    ],
+  },
 
-    posts: {
-        name: "Mohammedbrhan Abdelkadr",
-        avatar: "https://mui.com/static/images/avatar/1.jpg",
-        background_image: "https://source.unsplash.com/random?wallpapers",
-        post_data: [
-            {
-                id: 1,
-                timestamp: "2 hours ago",
-                description: "Enjoying a sunny day at the beach!",
-                image: "https://source.unsplash.com/random?wallpapers"
-            },
-            {
-                id: 2,
-                timestamp: "5 hours ago",
-                description: "Just finished reading this amazing book.",
-                type: "note",
-            },
-            {
-                id: 3,
-                timestamp: "2 hours ago",
-                description: "Enjoying a sunny day at the beach!",
-                image: "https://source.unsplash.com/random?wallpapers"
-            },
-            {
-                id: 4,
-                timestamp: "5 hours ago",
-                description: "Just finished reading this amazing book.",
-                type: "note",
-            },
-            {
-                id: 5,
-                author: "John Doe",
-                avatar: "/path-to-avatar1.jpg",
-                timestamp: "2 hours ago",
-                description: "Enjoying a sunny day at the beach!",
-                image: "https://source.unsplash.com/random?wallpapers"
-            },
-        ],
-    }
+  posts: {
+    name: "Mohammedbrhan Abdelkadr",
+    avatar: "profile-photo.png",
+    background_image: "https://source.unsplash.com/random?wallpapers",
+    post_data: [
+      {
+        id: 1,
+        timestamp: "2 hours ago",
+        description: "Enjoying a sunny day at the beach!",
+        image: "https://source.unsplash.com/random?wallpapers",
+      },
+      {
+        id: 2,
+        timestamp: "5 hours ago",
+        description: "Just finished reading this amazing book.",
+        type: "note",
+      },
+      {
+        id: 3,
+        timestamp: "2 hours ago",
+        description: "Enjoying a sunny day at the beach!",
+        image: "https://source.unsplash.com/random?wallpapers",
+      },
+      {
+        id: 4,
+        timestamp: "5 hours ago",
+        description: "Just finished reading this amazing book.",
+        type: "note",
+      },
+      {
+        id: 5,
+        author: "John Doe",
+        avatar: "/path-to-avatar1.jpg",
+        timestamp: "2 hours ago",
+        description: "Enjoying a sunny day at the beach!",
+        image: "https://source.unsplash.com/random?wallpapers",
+      },
+    ],
+  },
 };
 
 // Exporting the data object so that the App.js file can import it.
--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,148 +1,252 @@
-
 // React
-import React from 'react';
+import React from "react";
 
-// MUI 
-import Grid from '@mui/material/Grid';
-import Typography from '@mui/material/Typography';
-import Box from '@mui/material/Box';
-import Avatar from '@mui/material/Avatar';
-import IconButton from '@mui/material/IconButton';
-import Divider from '@mui/material/Divider';
-import List from '@mui/material/List';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import ListItemText from '@mui/material/ListItemText';
+// MUI
+import Grid from "@mui/material/Grid";
+import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
+import Avatar from "@mui/material/Avatar";
+import IconButton from "@mui/material/IconButton";
+import Divider from "@mui/material/Divider";
+import List from "@mui/material/List";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
 
 // Local
-import ResumeCard from './ResumeCard';
-
+import ResumeCard from "./ResumeCard";
 
 // ResumePortfolio: A React component that displays a resume/portfolio.
 export default function ResumePortfolio({ profile }) {
-    return (
-        <Box>
-            <Grid container spacing={2}>
+  return (
+    <Box>
+      <Grid container spacing={2}>
+        {/* Left Column */}
+        <Grid item xs={12} md={4}>
+          {/* Profile */}
+          <ResumeCard>
+            <Box
+              sx={{
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+              }}
+            >
+              <Avatar
+                alt={profile.name}
+                src={profile.avatar}
+                sx={{ width: 100, height: 100 }}
+              />
+              <Typography variant="h5" component="div" sx={{ mt: 2 }}>
+                {profile.name}
+              </Typography>
+              <Typography color="text.secondary" sx={{ mt: 1 }}>
+                {profile.address}
+              </Typography>
+              {/* ... other personal information */}
+            </Box>
 
-                {/* Left Column */}
-                <Grid item xs={12} md={4}>
+            {/* Contact Details */}
+            <Divider sx={{ mt: 2 }} />
+            <List component="nav" dense>
+              {profile.contacts.map((contact, index) => (
+                <ListItemButton key={index}>
+                  <ListItemIcon>
+                    <contact.icon />
+                  </ListItemIcon>
+                  <ListItemText
+                    primary={contact.label}
+                    secondary={contact.value}
+                  />
+                </ListItemButton>
+              ))}
+            </List>
+            <Divider sx={{ mb: 2 }} />
 
-                    {/* Profile */}
-                    <ResumeCard>
-                        <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-                            <Avatar alt={profile.name} src={profile.avatar} sx={{ width: 100, height: 100 }} />
-                            <Typography variant="h5" component="div" sx={{ mt: 2 }}>
-                                {profile.name}
-                            </Typography>
-                            <Typography color="text.secondary" sx={{ mt: 1 }}>
-                                {profile.address}
-                            </Typography>
-                            {/* ... other personal information */}
-                        </Box>
+            {/* Skills */}
+            <Typography variant="h6" component="div" sx={{ mt: 2 }}>
+              Skills
+            </Typography>
+            <List component="nav" dense>
+              {profile.skills.map((skill, index) => (
+                <ListItemButton key={index}>
+                  <ListItemText
+                    primary={skill.category}
+                    secondary={skill.skills}
+                  />
+                </ListItemButton>
+              ))}
+            </List>
+          </ResumeCard>
 
-                        {/* Contact Details */}
-                        <Divider sx={{ mt: 2 }} />
-                        <List component="nav" dense>
-                            {profile.contacts.map((contact, index) => (
-                                <ListItemButton key={index}>
-                                    <ListItemIcon>
-                                        <contact.icon />
-                                    </ListItemIcon>
-                                    <ListItemText primary={contact.label} secondary={contact.value} />
-                                </ListItemButton>
-                            ))}
-                        </List>
-                        <Divider sx={{ mb: 2 }} />
+          {/* Social Media Links */}
+          <ResumeCard>
+            <Typography
+              variant="h6"
+              component="div"
+              sx={{ textAlign: "center" }}
+            >
+              Social Media
+            </Typography>
+            <Box sx={{ mt: 2, textAlign: "center" }}>
+              {profile.socialMedia.map((media, index) =>
+                media.link ? (
+                  <IconButton
+                    color="primary"
+                    key={index}
+                    component="a"
+                    href={media.link}
+                    target="_blank"
+                  >
+                    <media.icon />
+                  </IconButton>
+                ) : (
+                  ""
+                )
+              )}
+            </Box>
+          </ResumeCard>
+        </Grid>
 
-                        {/* Skills */}
-                        <Typography variant="h6" component="div" sx={{ mt: 2 }}>
-                            Skills
-                        </Typography>
-                        <List component="nav" dense>
-                            {profile.skills.map((skill, index) => (
-                                <ListItemButton key={index}>
-                                    <ListItemText primary={skill.category} secondary={skill.skills} />
-                                </ListItemButton>
-                            ))}
-                        </List>
-                    </ResumeCard>
+        {/* Right Column */}
+        <Grid item xs={12} md={8}>
+          {/* Summary */}
+          <ResumeCard>
+            <Typography variant="h6" component="div">
+              Summary
+            </Typography>
+            <Box sx={{ mt: 2 }}>
+              <React.Fragment>
+                <Box sx={{ mb: 3 }}>
+                  <Typography variant="body2" sx={{ mt: 1 }}>
+                    {profile.summary}
+                  </Typography>
+                </Box>
+              </React.Fragment>
+            </Box>
+          </ResumeCard>
 
-                    {/* Social Media Links */}
-                    <ResumeCard>
-                        <Typography variant="h6" component="div" sx={{ textAlign: 'center' }}>
-                            Social Media
-                        </Typography>
-                        <Box sx={{ mt: 2, textAlign: 'center' }}>
-                            {profile.socialMedia.map((media, index) => (
-                                <IconButton color="primary" key={index} component="a" href={media.link}>
-                                    <media.icon />                                    
-                                </IconButton>
-                            ))}
-                        </Box>
-                    </ResumeCard>
-                </Grid>
+          {/* Work Experience */}
+          <ResumeCard>
+            <Typography variant="h6" component="div">
+              Work Experience
+            </Typography>
+            <Box sx={{ mt: 2 }}>
+              {profile.experience.map((experience, index) => (
+                <React.Fragment key={index}>
+                  <Box sx={{ mb: 3 }}>
+                    <Typography variant="subtitle1">
+                      {experience.title}
+                    </Typography>
+                    <Typography variant="body2" color="text.secondary">
+                      {experience.date}
+                    </Typography>
+                    <Typography variant="body2" sx={{ mt: 1 }}>
+                      {experience.description}
+                    </Typography>
+                  </Box>
 
-                {/* Right Column */}
-                <Grid item xs={12} md={8}>
+                  {/* Divider but not for single or last item */}
+                  {index !== profile.experience.length - 1 && (
+                    <Divider sx={{ mb: 2 }} />
+                  )}
+                </React.Fragment>
+              ))}
+            </Box>
+          </ResumeCard>
 
-                    {/* Education */}
-                    <ResumeCard>
-                        <Typography variant="h6" component="div">
-                            Education
-                        </Typography>
-                        <Box sx={{ mt: 2 }}>
-                            {profile.education.map((education, index) => (
-                                <React.Fragment key={index}>
-                                    <Box sx={{ mb: 3 }}>
-                                        <Typography variant="subtitle1">{education.degree}</Typography>
-                                        <Typography variant="body2" color="text.secondary">{education.date}</Typography>
-                                        <Typography variant="body2" sx={{ mt: 1 }}>
-                                            {education.school}
-                                        </Typography>
-                                        <Typography variant="body2" sx={{ mt: 1 }}>
-                                            {education.gpa}
-                                        </Typography>
-                                    </Box>
+          {/* Education */}
+          <ResumeCard>
+            <Typography variant="h6" component="div">
+              Education
+            </Typography>
+            <Box sx={{ mt: 2 }}>
+              {profile.education.map((education, index) => (
+                <React.Fragment key={index}>
+                  <Box sx={{ mb: 3 }}>
+                    <Typography variant="subtitle1">
+                      {education.degree}
+                    </Typography>
+                    <Typography variant="body2" color="text.secondary">
+                      {education.date}
+                    </Typography>
+                    <Typography variant="body2" sx={{ mt: 1 }}>
+                      {education.school}
+                    </Typography>
+                    <Typography variant="body2" sx={{ mt: 1 }}>
+                      {education.gpa}
+                    </Typography>
+                  </Box>
 
-                                    {/* Divider but not for single or last item */}
-                                    {index !== profile.education.length - 1 && (
-                                        <Divider sx={{ mb: 2 }} />
-                                    )}
+                  {/* Divider but not for single or last item */}
+                  {index !== profile.education.length - 1 && (
+                    <Divider sx={{ mb: 2 }} />
+                  )}
+                </React.Fragment>
+              ))}
+            </Box>
+          </ResumeCard>
 
-                                </React.Fragment>
-                            ))}
-                        </Box>
-                    </ResumeCard>
+          {/* Certifications */}
+          <ResumeCard>
+            <Typography variant="h6" component="div">
+              Certifications
+            </Typography>
+            <Box sx={{ mt: 2 }}>
+              {profile.certifications.map((certificate, index) => (
+                <React.Fragment key={index}>
+                  <Box sx={{ mb: 3 }}>
+                    <Typography variant="subtitle1">
+                      {certificate.title}
+                    </Typography>
+                    <Typography variant="body2" color="text.secondary">
+                      {certificate.date}
+                    </Typography>
+                    <Typography variant="body2" sx={{ mt: 1 }}>
+                      {certificate.description}
+                    </Typography>
+                  </Box>
 
-                    {/* Work Experience */}
-                    <ResumeCard>
-                        <Typography variant="h6" component="div">
-                            Work Experience
-                        </Typography>
-                        <Box sx={{ mt: 2 }}>
-                            {profile.experience.map((experience, index) => (
-                                <React.Fragment key={index}>
-                                    <Box sx={{ mb: 3 }}>
-                                        <Typography variant="subtitle1">{experience.title}</Typography>
-                                        <Typography variant="body2" color="text.secondary">{experience.date}</Typography>
-                                        <Typography variant="body2" sx={{ mt: 1 }}>
-                                            {experience.description}
-                                        </Typography>
-                                    </Box>
+                  {/* Divider but not for single or last item */}
+                  {index !== profile.certifications.length - 1 && (
+                    <Divider sx={{ mb: 2 }} />
+                  )}
+                </React.Fragment>
+              ))}
+            </Box>
+          </ResumeCard>
 
-                                    {/* Divider but not for single or last item */}
-                                    {index !== profile.experience.length - 1 && (
-                                        <Divider sx={{ mb: 2 }} />
-                                    )}
+          {/* awards */}
+          <ResumeCard>
+            <Typography variant="h6" component="div">
+              Awards
+            </Typography>
+            <Box sx={{ mt: 2 }}>
+              {profile.awards.map((award, index) => (
+                <React.Fragment key={index}>
+                  <Box sx={{ mb: 3 }}>
+                    <Typography variant="subtitle1">
+                      {award.title}
+                    </Typography>
+                    <Typography variant="body2" color="text.secondary">
+                      {award.date}
+                    </Typography>
+                    <Typography variant="body2" sx={{ mt: 1 }}>
+                      {award.description}
+                    </Typography>
+                  </Box>
 
-                                </React.Fragment>
-                            ))}
-                        </Box>
-                    </ResumeCard>
+                  {/* Divider but not for single or last item */}
+                  {index !== profile.awards.length - 1 && (
+                    <Divider sx={{ mb: 2 }} />
+                  )}
+                </React.Fragment>
+              ))}
+            </Box>
+          </ResumeCard>
 
-                </Grid>
- 
-            </Grid>
-        </Box>
-    );
+        </Grid>
+      </Grid>
+    </Box>
+  );
 }
